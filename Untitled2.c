af323a4c60d4fd5e2e76a03c4bc5d048b893a41d
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2021-12-09 08:44:53 +0100

    Add files via upload

--- Untitled2.c
+++ Untitled2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 void main(){
 int stranicaA=3,stranicaB=4,stranicaC=5;
+printf("unesi str\n");
+scanf("%d %d %d",&stranicaA,&stranicaB,&stranicaC);
 int volumen=stranicaA*stranicaB*stranicaC;
 printf("%d",volumen);
 }
