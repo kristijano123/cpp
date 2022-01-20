7042804a7b641e707f3cc7075e3478722f337187
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-01-20 08:23:21 +0100

    Add files via upload

--- Untitled1.c
+++ Untitled1.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 int main()
 {
-
-       int rf;
-       int rt;
-       printf("unesi polumjer fontane\n");
-       printf("unesi polumjer travnjaka\n");
-       scanf("%d %d",&rf,&rt);
-       float pf=rf*rf*3.14159 ;
-       float pt=(rt*rt*3.14159)-pf;
-
-       printf("povrsina je %.2f",pt);
-
-return 0;
-
+    float x;
+    float y;
+    printf("unesi pocetnu cijenu\n");
+    scanf("%f",&x);
+    printf("unesi konacnu cijenu\n");
+    scanf("%f",&y);
+    float s=(x-y)/x*100;
+    printf("snizenje je %2.f %%\n",s);
+    return 0;
 }
