cd0d1fbb17c4c01b88c1710ed782c57ef2637ed7
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-02-03 07:27:53 +0100

    Add files via upload

---
+++ novi zadatak.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+   int a;
+   printf("unesi neki broj\n");
+   scanf("%d",&a);
+   int x=(((a*4/5)*4/5)*4/5);
+   printf("sad ima %d\n",x);
+
+
+
+
+}
+
+
