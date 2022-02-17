7d48141ff03aefecd9f6ced6dd37d3dea9c0df44
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-02-17 08:41:04 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -3,18 +3,24 @@
 
 int main()
 {
-    int x;
-    int y;
-    printf("unesi neku sekundu\n");
-    scanf("%d",&x);
-    printf("Unesi neku minutu\n");
-    scanf("%d",&y);
-    if(x>59){
-            printf("nesipravan unos");
-    }else{
-    float a=(float)((float)x/60+y)/60;
-    printf("sati je %.3f",a);
-    }
 
+    int a;
+    int b;
+    int c;
+    printf("unesi neki broj\n");
+    scanf("%d",&a);
+    printf("unesi neki broj\n");
+    scanf("%d",&b);
+    printf("unesi neki broj\n");
+    scanf("%d",&c);
+    if (a+b==c){
+        printf("%d + %d = %d",a,b,a+b);
+        }else if (a+c==b){
+        printf("%d+ %d= %d",a,c,b);
+        }else if (c+b==a){
+        printf("%d+ %d= %d",c,b,a);
+        }else{
+            printf("niti jedan od ucitanih brojeva nije moguce prikazati kao zbroj");
+        }
 
 }
