3802bb96c19c7da0a02a386198ad3aeb3fa4c908
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-02-17 08:19:47 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main()
-{ float a;
-  float b;
-  float c;
-  printf("unesi broj stupova\n");
-  scanf("%f",&a);
-  printf("unesi udaljenost izmedu stupova\n");
-  scanf("%f",&b);
-  printf("unesi sirinu stupa\n");
-  scanf("%f",&c);
-  float e=(b*100)*(a-1)+c*(a-2);
-  printf("udaljenost izmedu prvog izadnjeg stupa je\n%2.f",e);
+{
+    int x;
+    int y;
+    printf("unesi neku sekundu\n");
+    scanf("%d",&x);
+    printf("Unesi neku minutu\n");
+    scanf("%d",&y);
+    if(x>59){
+            printf("nesipravan unos");
+    }else{
+    float a=(float)((float)x/60+y)/60;
+    printf("sati je %.3f",a);
+    }
 
-    return 0;
+
 }
