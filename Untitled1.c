6bdafa97d15f4e0b83bba2cae5bd71b8fe602e48
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-03-03 10:29:57 +0100

    Add files via upload

--- Untitled1.c
+++ Untitled1.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
-int main()
-{
-    float x;
-    float y;
-    printf("unesi pocetnu cijenu\n");
-    scanf("%f",&x);
-    printf("unesi konacnu cijenu\n");
-    scanf("%f",&y);
-    float s=(x-y)/x*100;
-    printf("snizenje je %2.f %%\n",s);
-    return 0;
-}
