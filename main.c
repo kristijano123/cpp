75fb43642906683151cb36e9d6f8d9b8929f7808
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-03-10 07:47:39 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 
 int main()
 {
-   int x;
-   printf("unesi neki broj\n");
-   scanf("%d",&x);
-   if (x%2==0)
-    printf("broj je paran\n");
-    if (x%2==1)
-    printf("broj je neparan");
-
+    int x;
+    int y;
+    printf("unesi neki broj\n");
+    scanf("%d",&x);
+     printf("unesi neki broj\n");
+    scanf("%d",&y);
+    if (x<y)
+        printf("broj %d je veci od %d",y,x);
+    else if (x>y)
+        printf("broj %d je veci od %d",x,y);
 }
