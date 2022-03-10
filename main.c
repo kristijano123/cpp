8a7715eaf3f09a24de492f2eef7b279ff9e45aa0
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-03-10 08:26:46 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main()
 {
-     int x;
-     printf("unesi neki troznamenkasti broj\n");
-     scanf ("%d",&x);
-     int y=x/100;
-     int a=x/10-y*10;
-     int b=x%10;
-     if(y*y*y+a*a*a+b*b*b==x)
-        printf("to je armstrongov broj");
-     else
-        printf("to nije armstrongov broj");
+    int x;
+    printf("unesi neki kvadrant1\n");
+    scanf("%d",&x);
+    if (x==1)
+    printf("++");
+    else if(x==2)
+        printf("-+");
+    else if (x==3)
+    printf("--");
+    else if (x==4)
+        printf("+-");
+    else
+        printf("to nije kvadrant");
+
 
 }
