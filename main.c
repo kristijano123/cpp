24be0c92871f08825896795fb153892db66d9633
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-03-10 07:37:14 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,55 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main()
 {
-    printf(" _______________________________________\n");
-    printf("|\t\t\t\t\t|\n");
-    printf("|\t 1. kvadrat\t\t\t|\n");
-    printf("|\t\t\t\t\t|\n");
-    printf("|\t 2. pravokutnik\t\t\t|\n");
-    printf("|\t\t\t\t\t|\n");
-    printf("|\t 3. krug\t\t\t|\n");
-    printf("|\t\t\t\t\t|\n");
-    printf(" _______________________________________\n");
+   int x;
+   printf("unesi neki broj\n");
+   scanf("%d",&x);
+   if (x%2==0)
+    printf("broj je paran\n");
+    if (x%2==1)
+    printf("broj je neparan");
 
-
-    int x;
-    int a;
-    int b;
-    int c;
-    int d;
-
-    printf("unesi odabranu opciju\n");
-    scanf("%d",&x);
-
-
-
-    switch(x){
-    case 1:
-    printf("unesi stranicu kvadrata\n");
-    scanf("%d",&a);
-    int pk=a*a;
-    printf("povrsina kvadrata je %d",pk);
-    break;
-    case 2:
-    printf("unesi stranicu pravokutnika \n");
-    scanf("%d",&b);
-    printf("unesi stranicu pravokutnika\n");
-    scanf("%d",&c);
-    int pp=b*c;
-    printf("povrsina pravokutnika je %d",pp);
-    break;
-    case 3:
-      printf("unesi polumjer kruga\n");
-    scanf("%d",&d);
-    float p=(float)(d*d)*3.14;
-    printf("povrsina kruga je %.2f",p);
-    break;
-
-    default:
-        printf("nije dobar broj");
-        break;
-    }
 }
