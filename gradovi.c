0639bfb2de84ae94fb2b3d83040927433e21983c
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-04-28 07:40:05 +0200

    Add files via upload

---
+++ gradovi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int n;
+   int sum=0;
+   printf("unesi udaljenost između dva grada\n");
+   scanf("%d",&n);
+   while(n-1){
+   scanf("%d",&dist);
+   sum+=dist;
+   n--;
+   }
+    printf("udaljenost između prvog i zadnjeg grada je %d km",sum);
+
+
+
+}
