24190d96c86127ba4f05b7b2db0a5e256f70deed
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-05-26 08:34:48 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <ctype.h>
 int main()
 {
-    int x;
-    printf(" unesi neki broj kvadanta\n");
-    scanf("%d",&x);
-    switch(x){
-case 1:
-    printf("++");
-    break;
-case 2:
-    printf("-+");
-    break;
-case 3:
-    printf("--");
-    break;
-case 4:
-    printf("+-");
-    break;
-default:
-    printf ("to nije kvadrant");
+    printf("------------------\n");
+    printf("|\t\t\t|\n");
+    printf("|\t 1.mnozenje\t|\n");
+    printf("|\t\t\t|\n");
+    printf("|\t 2.dijeljenje\t|\n");
+    printf("|\t\t\t|\n");
+    printf("|\t 3.zbrajanje|\n");
+    printf("|\t\t\t|\n");
+    printf("|\t 4.oduzimanje|\n"):
+    printf("|\t\t\t|\n");
+    printf("------------------\n");
+
+
+    int a,b;
+    char unos[10];
+    printf("odaberite operaciju");
+    scanf("%s",unos);
+
+    for(int i=0;unos[i]!='\0';i++){
+        unos[i]=tolower(unos[i]);
+
+    printf("unesi 2 broja");
+    scanf("%d %d",&a,&b);
+    float rezultat;
+
+    if(strcmp(unos,"mnozenje")){//do mnozenje
+        rezultat=a*b;
     }
+    if(strcmp(unos,"dijeljenje")){
+        //do dijeljenje
+    }   rezultat=a/b
+     if(strcmp(unos,"zbrajanje")){//do zbrajanje
+        rezultat=a+b
+    }
+     if(strcmp(unos,"oduzimanje")){//do oduzimanje
+        rezultat=a-b
+    }
+
+
+    printf("rezultat je %.2f",rezultat);
+
 }
