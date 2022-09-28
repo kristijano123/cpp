c6f3e66fa8c91674c72dfd2db239ee871dd5f6e8
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-09-28 10:37:15 +0200

    Add files via upload

---
+++ da.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int da(int a,int b,int c)
+{
+   int f2;
+   f2=a+b;
+
+    if(c==1||c==2)return 1;;
+    if(c==3)return f2;
+    else return da(b,f2,c-1);
+}
+
+int main()
+{
+    int a;
+    printf("unesi neki broj");
+    scanf("%d",&a);
+
+
+    printf("%d",da(1,1,a));
+
+}
