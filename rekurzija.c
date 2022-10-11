ec5624f3584a6a1b355019d6489751f62ba362fd
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-10-11 07:48:56 +0200

    Add files via upload

---
+++ rekurzija.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int rekurzija(int a,int n){
+
+return n==1?a:rekurzija(a*5,--n);
+
+}
+int main()
+{
+    int n;
+    printf("unesi neki broj");
+    scanf("%d",&n);
+    printf("%d",rekurzija(1,n));
+}
