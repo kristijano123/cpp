10c7f48247ffacf2a5465cfa079af2f3bd108e7f
kristijano123 <95843664+kristijano123@users.noreply.github.com>
2022-10-12 09:19:14 +0200

    Add files via upload

--- kalkulator.c
+++ kalkulator.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+float calc(float a,char o, float b){
+if(o=='-')
+    return (a-b);
+else if (o=='+')
+    return (a+b);
+else if (o=='*')
+    return (a*b);
+else
+    return (a/b);
+}
+
 
 int main()
 {
-    int B1;
-    int N1;
-    int B2;
-    int N2;
-    printf("unesi brojnik 1\n");
-    scanf("%d",&B1);
-    printf("unesi nazivnik 1\n");
-    scanf("%d",&N1);
-    printf("unesi brojnik 2\n");
-    scanf("%d",&B2);
-     printf("unesi nazivnik 2\n");
-    scanf("%d",&N2);
-    int N3=N2*N1;
-    int b1=N2*B1;
-    int b2=N1*B2;
-    int B3=b1+b2;
-    float x=(float)B3/N3;
-    printf("rezultat u decimalnom zapisu je %.2f\n",x);
-    printf("rezultat u razlomku je %d / %d\n",B3,N3);
+   float a;
+   char o;
+   float b;
+   printf("unesi neki broj");
+   scanf("%f",&a);
+   printf("unesi neku mat operaciju");
+   scanf(" %c",&o);
+   printf("unesi neki broj");
+   scanf("%f",&b);
+   printf("rez je %f",calc(a,o,b));
 
+return 0;
 }
